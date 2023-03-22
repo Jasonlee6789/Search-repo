0a026459c8537adafb2de7ea408f1b24de043240
Jeff <li.jing@smartcompany.jp>
2023-03-22 21:18:59 +0900

    feat: I finish the github repo fetch part.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import { IRepos } from './IRepos'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [input, setInput] = useState('')
+	const [repos, setRepos] = useState<IRepos[]>([])
+
+	const search = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault()
+		const form = event.target as HTMLFormElement
+		const input = form.querySelector('#searchText') as HTMLInputElement
+		setInput(input.value)
+		searchForRepos(input.value)
+		input.value = ''
+	}
+
+	const searchForRepos = async (query: String): Promise<IRepos[]> => {
+		const result = await fetch(
+			'https://api.github.com/search/repositories' +
+				(query ? `?q=${query}` : "?q=''")
+		)
+		const repos = await result.json()
+		return repos.items
+	}
+
+	useEffect(() => {
+		;(async () => {
+			const query = encodeURIComponent(input)
+			const response = await searchForRepos(query)
+			setRepos(response)
+		})()
+	}, [])
+
+	return (
+		<div className="App">
+			<h1>Github Repositories Search App</h1>
+			<form className="searchForm" onSubmit={(event) => search(event)}>
+				<input id="searchText" type="text" />
+				<button>Search</button>
+			</form>
+		</div>
+	)
 }
 
-export default App;
+export default App
