20a3722660c43aee05e480f2907a92264eb56912
Jeff <li.jing@smartcompany.jp>
2023-03-23 09:30:39 +0900

    fix: modify the props

--- src/components/Repos.tsx
+++ src/components/Repos.tsx
@@ -1,29 +1,31 @@
 import { IRepos } from '../IRepos'
-
-export default function Repos({ reposList }: { reposList: IRepos[] }) {
+interface IReposProps {
+	reposList: IRepos[]
+}
+export default function Repos({ reposList }: IReposProps) {
 	return (
 		<>
-			{reposList[0] && <div>{reposList[0].language}</div>}
-
-			{reposList.map((repo) => {
-				const {
-					id,
-					git_url,
-					stargazers_count,
-					full_name,
-					description,
-					created_at,
-				} = repo
-
-				return (
-					<div key={id}>
-						<a href={git_url}>{full_name}</a>
-						<p>{description}</p>
-						<p>Created: {created_at}</p>
-						<p>Stars: {stargazers_count}</p>
-					</div>
-				)
-			})}
+			{reposList.length &&
+				reposList.map((repo) => {
+					const {
+						id,
+						git_url,
+						stargazers_count,
+						full_name,
+						owner,
+						description,
+						created_at,
+					} = repo
+					return (
+						<div key={id}>
+							Repo name: <a href={git_url}>{full_name}</a>
+							<p>Description: {description}</p>
+							<p>Repo Owner: {owner.login}</p>
+							<p>Created: {created_at}</p>
+							<p>Stars: {stargazers_count}</p>
+						</div>
+					)
+				})}
 		</>
 	)
 }
